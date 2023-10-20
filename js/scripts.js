9a7586e662299d81e36896b47ffca08f7c6152d6
Jack Williams <jackwillpdx@gmail.com>
2023-10-20 16:09:53 -0700

    added java

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
-window.onload = function () {
-  const form = document.querySelector("form");
-  form.onsubmit = function (event) {
-      event.preventDefault();
+function showSpecies()
 
-      
+
+
+window.onload = function () {
+  let form = document.querySelector("form");
+  form.onclick = function (event) {
+    document.querySelector ("#species").removeAttribute("class")
+    document.querySelector ("#film").removeAttribute("class");
+    document.querySelector ("#music").removeAttribute("class");
+    document.querySelector ("#color").removeAttribute("class");
+    event.preventDefault();
+  
+    }
   }
-}
