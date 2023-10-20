5763abfc4c00d9e7dbb9f80e376df8ece49bc191
Jack Williams <jackwillpdx@gmail.com>
2023-10-20 11:31:01 -0700

    hide elements

--- js/scrpits.js
+++ js/scrpits.js
@@ -1,19 +1,10 @@
 window.onload = function () {
-    function hideQuestions() {
-    let div = document.querySelector("species");
-    if (div.style.display === "none") {
-      div.style.display = "block";
-    } else {
-      div.style.display = "none";
-    }  
-  }  
-}
+  const form = document.querySelector("form");
+  form.onsubmit = function (event) {
+      event.preventDefault();
 
-function handleSelect(event) {
-    event.preventDefault();
-    const selection = document.getElementById("evening").value;
-  }
-  
-  window.addEventListener("load", function() {
-    document.getElementById("night-out").addEventListener("submit", handleSelect);
-  });
+      function hide() {
+        document.getElementById("species").style.display ="hidden"
+    }
+  }  
+};
