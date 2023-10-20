8319ecf71c9dd7afd7b1dc5c560bda867c8b81e9
Jack Williams <jackwillpdx@gmail.com>
2023-10-20 13:01:14 -0700

    after lunch commit

--- js/scrpits.js
+++ js/scrpits.js
@@ -2,10 +2,5 @@ window.onload = function () {
   const form = document.querySelector("form");
   form.onsubmit = function (event) {
       event.preventDefault();
-
-      document.querySelector("#species").addEventListener("submit", function()){
-        document.querySelector(#film).style.visibility="visible";
-        document.querySelector("#species").style.visibility="hidden";
-      }
-    }
- };
+  }
+};
