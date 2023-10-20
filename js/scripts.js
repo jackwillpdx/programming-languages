8757f50ca2842b7be5d82e50407b87eb33642423
Jack Williams <jackwillpdx@gmail.com>
2023-10-20 14:19:47 -0700

    java trail and error

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,5 @@ window.onload = function () {
   const form = document.querySelector("form");
   form.onsubmit = function (event) {
       event.preventDefault();
-
-      let btn1 = document.getElementById("btn1");
-      if (button = form.onsubmit) {
-        document.querySelector('#species').removeAttribute("class");
-      } else {
-        document.querySelector('#animal').removeAttribute("class");
-      }
-
-      let btn2 = document.getElementById("btn2")
-      if (button = form.onsubmit) {
-        document.querySelector('#film').removeAttribute("class");
-      } else {
-        document.querySelector('#movie').removeAttribute("class");
-      }
   }
-};
+}
